95d875e523aa0659169b980417809342f77a443b
Matthew DeCarlo <matthewwho@me.com>
2014-10-20 21:06:20 -0400

    updated blog index

---
+++ js/WebFontConfig.js
@@ -0,0 +1,12 @@
+  WebFontConfig = {
+    google: { families: [ 'Oswald:300,700:latin', 'Roboto+Condensed:400,300,700:latin', 'Roboto:400,100,500:latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })();
---
+++ js/navigation.js
@@ -0,0 +1,3 @@
+	$(function() {
+	alert( "Thanks for visiting!" );
+	});
